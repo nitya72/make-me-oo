12a8f761282c501a5e1c9fd4a913246007ddfc17
Nitya Arora <nityaarora72@gmail.com>
2021-01-18 17:51:54 +0530

    [Refactor] removed getter for Point, all tests running

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -9,21 +9,13 @@ public class Point {
         this.y = y;
     }
 
-    public double getX() {
-        return x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
     public double distanceFromOrigin() {
         return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
     }
 
     public double distanceFromAnotherPoint(Point point) {
-        double xDistance = x - point.getX();
-        double yDistance = y - point.getY();
+        double xDistance = x - point.x;
+        double yDistance = y - point.y;
         return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
     }
 
@@ -32,8 +24,8 @@ public class Point {
     }
 
     public double directionFromAnotherPoint(Point point) {
-        double xDistance = x - point.getX();
-        double yDistance = y - point.getY();
+        double xDistance = x - point.x;
+        double yDistance = y - point.y;
         return Math.atan2(yDistance, xDistance);
     }
 }
